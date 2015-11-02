9f3c396e7044e524c6976d0ca5809546d636647b
Awethon <gorod.63@yandex.ru>
2015-11-03 02:28:13 +0300

    @Pyroblasty helped me a lot. Looks pretty nice.

---
+++ masteries/language.js
@@ -0,0 +1,11 @@
+// In order to translate the calculator to another language, also change data.js and index.html
+// This file contains strings used for display purposes in calculator.js
+
+var languagePack_English = {
+    tree_names: ["Свирепость", "Коварство", "Решимость"],
+    rank: "Ранг",
+    requires: "Требуется",
+    points_in: "очк. в",
+    swap_warning: "Выбор этого таланта отнимет очки из",
+    doubleclick_to_reset: "Двойной клик для сброса этой ветки",
+};
